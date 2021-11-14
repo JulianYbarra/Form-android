74f31fbd5aab4c5ddac51c33fc20f037b50c110e
JulianYbarra <julianybarra95@gmail.com>
2021-11-13 22:41:20 -0300

    Feature | Login Facebook | JY

--- app/src/main/java/com/junka/form/login/LoginActivity.kt
+++ app/src/main/java/com/junka/form/login/LoginActivity.kt
@@ -3,24 +3,98 @@ package com.junka.form.login
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.junka.form.databinding.ActivityLoginBinding
+import com.junka.form.main.MainActivity
 
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
-    private lateinit var navController: NavController
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
+
+    private val fbCallbackManager = CallbackManager.Factory.create()
+
+    override fun onStart() {
+        super.onStart()
+
+        auth.currentUser?.let {
+            MainActivity.create(this)
+            finish()
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.loginButton.apply {
+            setPermissions("email", "public_profile")
+            registerCallback(fbCallbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Log.d(TAG, "facebook:onSuccess:$loginResult")
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "facebook:onCancel")
+                    // ...
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "facebook:onError", error)
+                    // ...
+                }
+            })
+        }
     }
 
-    companion object{
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    MainActivity.create(this)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    fun singOut() {
+        Firebase.auth.signOut()
+    }
+
+    companion object {
         private val TAG = this::class.java.name
     }
 }
--- app/src/main/java/com/junka/form/main/MainActivity.kt
+++ app/src/main/java/com/junka/form/main/MainActivity.kt
@@ -1,7 +1,28 @@
 package com.junka.form.main
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import com.junka.form.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
 
+
+    companion object{
+        fun create(context : Context){
+            Intent(context,MainActivity::class.java).run{
+                startActivity(context,this,null)
+            }
+        }
+    }
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
